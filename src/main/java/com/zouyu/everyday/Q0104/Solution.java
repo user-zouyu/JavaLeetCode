f8afcda0eb10abf8a18fe87782e8b88582a66426
user-zouyu <user.zouyu@foxmail.com>
2022-09-27 13:39:23 +0800

    leetcode Q0104

---
+++ src/main/java/com/zouyu/everyday/Q0104/Solution.java
@@ -0,0 +1,26 @@
+package com.zouyu.everyday.Q0104;
+
+/**
+ * @author ZouYu 2022/9/27 12:25
+ * @version 1.0.0
+ */
+class Solution {
+    public boolean CheckPermutation(String s1, String s2) {
+        if(s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] hash = new int[256];
+        for (int i = 0; i < s1.length(); i++) {
+            hash[s1.charAt(i)]++;
+            hash[s2.charAt(i)]--;
+        }
+
+        for (int i : hash) {
+            if(i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
