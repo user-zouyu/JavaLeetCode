3127e00951d54f54774fed18511e5c8b7e237a1f
user-zouyu <user.zouyu@foxmail.com>
2022-11-01 09:59:11 +0800

    Q005662

---
+++ src/main/java/com/zouyu/hot100/Q0062/Q0062Test.java
@@ -0,0 +1,36 @@
+package com.zouyu.hot100.Q0062;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ZouYu 2022/11/1 9:41
+ * @version 1.0.0
+ */
+public class Q0062Test {
+    private static final Solution solution = new Solution1();
+
+
+    @Test
+    void test1() {
+
+        int uniquePaths = solution.uniquePaths(3, 2);
+
+        System.out.println(uniquePaths);
+
+        Assertions.assertEquals(uniquePaths, 3);
+
+    }
+
+    @Test
+    void test2() {
+
+        int uniquePaths = solution.uniquePaths(10, 10);
+
+        System.out.println(uniquePaths);
+
+        Assertions.assertEquals(uniquePaths, 48620);
+
+    }
+
+}
---
+++ src/main/java/com/zouyu/hot100/Q0062/Solution.java
@@ -0,0 +1,15 @@
+package com.zouyu.hot100.Q0062;
+
+/**
+ * @author ZouYu 2022/11/1 9:33
+ * @version 1.0.0
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+        long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return (int) ans;
+    }
+}
